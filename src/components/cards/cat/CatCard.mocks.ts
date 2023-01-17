42b5fb4cde5bbb2e2bf1efe5138b2f7b8cadf5d5
Leonardo <leonardo.zerino@peopleinteractive.com.br>
2023-01-17 15:36:04 -0300

    feat: import de font via css em detrimento de html

--- src/components/cards/cat/CatCard.mocks.ts
+++ src/components/cards/cat/CatCard.mocks.ts
@@ -3,7 +3,7 @@ import { ICatCard } from './CatCard';
 const base: ICatCard = {
   tag: 'Felines',
   title: `What's new in Cats`,
-  body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi perferendis molestiae non nemo doloribus. Doloremque, nihil! At ea atque quidem!',
+  body: 'Lorem Yyyy ipsum dolor sit amet consectetur adipisicing elit. Sequi perferendis molestiae non nemo doloribus. Doloremque, nihil! At ea atque quidem!',
   author: 'Alex',
   time: '2h ago'
 };

1ffedb822166d36d3d80109f474157d26f9ac5c0
zerinoid <leo.zerino@gmail.com>
2023-07-05 00:55:39 -0300

    chore: next, tailwind and typescript config adjustment

--- next.config.js
+++ next.config.js
@@ -1,10 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
   images: {
-    domains: ['i.pravatar.cc'],
+    domains: ['i.pravatar.cc']
   }
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;

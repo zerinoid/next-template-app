094ae9492fb8672742775dd53232fb9f41791391
zerinoid <leo.zerino@gmail.com>
2023-07-06 14:04:25 -0300

    refactor: implement next 13's font optmization

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,14 +4,7 @@ class MyDocument extends Document {
   render() {
     return (
       <Html>
-        <Head>
-          <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" />
-          <link
-            href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300..700&display=swap"
-            rel="stylesheet"
-          />
-        </Head>
+        <Head></Head>
         <body>
           <Main />
           <NextScript />
